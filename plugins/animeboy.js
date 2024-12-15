c84d752a21f69dc929e5d9a5df3915199f01b704
randima11 <Randimarandima175@gmail.com>
2024-12-15 17:48:46 +0530

    Update animeboy.js

--- plugins/animeboy.js
+++ plugins/animeboy.js
@@ -10,25 +10,3 @@ whatsapp number:94721551183
 */
 
 
-const axios = require('axios');
-const { cmd, commands } = require('../command');
-
-cmd({
-    pattern: "animeboy",
-    desc: "Fetch a random anime boy image.",
-    category: "fun",
-    react: "🧍",
-    filename: __filename
-},
-async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        const apiUrl = `https://api.waifu.pics/sfw/waifu`;
-        const response = await axios.get(apiUrl);
-        const data = response.data;
-
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '*©ᴅᴀʀᴋ_Qᴜᴇᴇɴ-ᴠ1 ʀᴀɴᴅᴏᴍ ᴀɴɪᴍᴇ ʙᴏʏ ɪᴍᴀɢꜱ ᴄʀᴇᴀᴛᴇ ʙʏ ᴍᴀɴɪꜱʜᴀ*' }, { quoted: mek });
-    } catch (e) {
-        console.log(e);
-        reply(`*Error Fetching Anime Boy image*: ${e.message}`);
-    }
-});
