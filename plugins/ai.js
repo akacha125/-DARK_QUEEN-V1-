76c4a04391628d08d88984df0a4c5340ab1518c7
randima11 <Randimarandima175@gmail.com>
2024-12-15 16:46:15 +0530

    🧚‍♂️DARK_QUEEN-V1🧚‍♂️

--- plugins/ai.js
+++ plugins/ai.js
@@ -9,24 +9,24 @@ creare by manisha sasmitha
 whatsapp number:94721551183
 */
 
-
 const config = require('../config')
 const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
+
 cmd({
-    pattern: "alive",
-    react: "👋",
-    desc: "Check bot online or no.",
+    pattern: "ai",
+    alias: ["gpt","bot"], 
+    react: "🧠",
+    desc: "ai chat.",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
+return reply(`${data.data}`)
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
-
-//========================
-
--- plugins/animeboy.js
+++ plugins/animeboy.js
@@ -10,23 +10,25 @@ whatsapp number:94721551183
 */
 
 
-const config = require('../config')
-const {cmd , commands} = require('../command')
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+
 cmd({
-    pattern: "alive",
-    react: "👋",
-    desc: "Check bot online or no.",
-    category: "main",
+    pattern: "animeboy",
+    desc: "Fetch a random anime boy image.",
+    category: "fun",
+    react: "🧍",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const apiUrl = `https://api.waifu.pics/sfw/waifu`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
 
-//========================
-
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '*©ᴅᴀʀᴋ_Qᴜᴇᴇɴ-ᴠ1 ʀᴀɴᴅᴏᴍ ᴀɴɪᴍᴇ ʙᴏʏ ɪᴍᴀɢꜱ ᴄʀᴇᴀᴛᴇ ʙʏ ᴍᴀɴɪꜱʜᴀ*' }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`*Error Fetching Anime Boy image*: ${e.message}`);
+    }
+});
